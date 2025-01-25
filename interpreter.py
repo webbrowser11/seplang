19ea9a36a4b05de55380b62c8a32a9768a0fad77
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-25 12:45:05 -0800

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,24 @@
+# Literally can only compile a string for now. I still learned stuff though :D
+
+import re
+
+program = r"C:\Users\terps\Documents\Python-Scripts\Big-projects\Instruction\program.txt"
+lines = []
+
+with open(program, "r") as file:
+    for line in file:
+        lines.append(line.strip())
+
+processedlines = []
+
+for line in lines:
+    line = re.sub(r'2(?=[a-zA-Z])', '', line)
+    line = re.sub(r'\b2\b', ' ', line)
+    processedlines.append(line)
+
+out = ''
+
+for char in processedlines:
+    out += ''+char
+
+print(out)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# The sep language
+To be used in the plank kernel
+
+# What it can do
+For now it can only read a file and compile a string based off that file. But soon it will have more functions, and the ability to detect the function and shift output based off the function.
